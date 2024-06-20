b4f1339b7aa583794d43581c43510253b2763c7f
devrogerzen <devrogerzen@gmail.com>
2024-06-20 02:46:45 -0300

    refactor: Update App.tsx to use Home component for main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,13 @@
 import "./App.css";
-import CoverPage from "./components/CoverPage";
-import DisplayItems from "./components/DisplayItems";
-import Header from "./components/Header";
-import coverPicture from "./assets/img.png";
 
-import {
-  ApiKey,
-  trending,
-  upcoming,
-  popular,
-  topRated,
-  now_playing,
-  trendingShows,
-  popularShows,
-  top_Rated_Shows,
-  airing_today,
-} from "./modules/ApiLinks";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-  const itemsProps = {
-    numberOfMovies: 16,
-  };
-
   return (
     <>
       <Header />
-      <br />
-      <br />
-      <br />
-      <br />
-      <CoverPage
-        title={"Hello"}
-        description={"lorenmsf fetrwe"}
-        catchyPhrase={"wegwe we twet"}
-        headerImage={coverPicture}
-        showSearch={true}
-        showHeaderImage={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${trending}?api_key=${ApiKey}`}
-        itemHeading="trending"
-        tvShowsOn={false}
-        moviesOn={true}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${upcoming}?api_key=${ApiKey}`}
-        itemHeading="upcoming"
-        tvShowsOn={false}
-        moviesOn={true}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${popular}?api_key=${ApiKey}`}
-        itemHeading="popular"
-        tvShowsOn={false}
-        moviesOn={true}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${topRated}?api_key=${ApiKey}`}
-        itemHeading="topRated"
-        tvShowsOn={false}
-        moviesOn={true}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${now_playing}?api_key=${ApiKey}`}
-        itemHeading="now_playing"
-        tvShowsOn={false}
-        moviesOn={true}
-        showButtons={true}
-      />
-      /*SHOWS*/
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${trendingShows}?api_key=${ApiKey}`}
-        itemHeading="trendingShows"
-        tvShowsOn={true}
-        moviesOn={false}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${popularShows}?api_key=${ApiKey}`}
-        itemHeading="popularShows"
-        tvShowsOn={true}
-        moviesOn={false}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${top_Rated_Shows}?api_key=${ApiKey}`}
-        itemHeading="top_Rated_Shows"
-        tvShowsOn={true}
-        moviesOn={false}
-        showButtons={true}
-      />
-      <DisplayItems
-        {...itemsProps}
-        apiEndpoint={`${airing_today}?api_key=${ApiKey}`}
-        itemHeading="airing_today"
-        tvShowsOn={true}
-        moviesOn={false}
-        showButtons={true}
-      />
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,115 @@
+import CoverPage from "../components/CoverPage";
+import DisplayItems from "../components/DisplayItems";
+import coverPicture from "../assets/img.png";
+import {
+  ApiKey,
+  trending,
+  upcoming,
+  popular,
+  topRated,
+  now_playing,
+  trendingShows,
+  popularShows,
+  top_Rated_Shows,
+  airing_today,
+} from "../modules/ApiLinks";
+
+
+
+const Home = () => {
+  return (
+    <div>
+
+
+<br />
+      <br />
+      <br />
+      <br />
+      <CoverPage
+        title={"Hello"}
+        description={"lorenmsf fetrwe"}
+        catchyPhrase={"wegwe we twet"}
+        headerImage={coverPicture}
+        showSearch={true}
+        showHeaderImage={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${trending}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="trending"
+        tvShowsOn={false}
+        moviesOn={true}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${upcoming}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="upcoming"
+        tvShowsOn={false}
+        moviesOn={true}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${popular}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="popular"
+        tvShowsOn={false}
+        moviesOn={true}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${topRated}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="topRated"
+        tvShowsOn={false}
+        moviesOn={true}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${now_playing}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="now_playing"
+        tvShowsOn={false}
+        moviesOn={true}
+        showButtons={true}
+      />
+      /*SHOWS*/
+      <DisplayItems
+        apiEndpoint={`${trendingShows}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="trendingShows"
+        tvShowsOn={true}
+        moviesOn={false}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${popularShows}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="popularShows"
+        tvShowsOn={true}
+        moviesOn={false}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${top_Rated_Shows}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="top_Rated_Shows"
+        tvShowsOn={true}
+        moviesOn={false}
+        showButtons={true}
+      />
+      <DisplayItems
+        apiEndpoint={`${airing_today}?api_key=${ApiKey}`}
+        numberOfMovies= {8}
+        itemHeading="airing_today"
+        tvShowsOn={true}
+        moviesOn={false}
+        showButtons={true}
+      />
+
+
+    </div>
+  )
+}
+
+export default Home
